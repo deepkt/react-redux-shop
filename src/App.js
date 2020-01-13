157144f1b9c64173f8edf43da8a9f31c02b007f8
pradeep.thoma <pradeep.thoma@compassitesinc.com>
2020-01-13 17:27:22 +0530

    Changed to Hash router to enable github pages relative domain routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, HashRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
 import { Home, Cart } from './containers';
 import { Header, Footer } from './components';
